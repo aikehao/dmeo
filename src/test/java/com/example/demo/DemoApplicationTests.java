d938c282451b398c28e22d4fac0c8e532b3e35cf
aikehao <2426671770@qq.com>
2023-04-17 16:12:29 +0800

    加回test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class DemoApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
