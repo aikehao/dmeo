91df701038d33a198e3b5b9202f6db539db406e1
aikehao <2426671770@qq.com>
2023-04-17 11:21:58 +0800

    demo

---
+++ src/main/java/com/example/demo/controller/Demo.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Demo {
+
+    @RequestMapping("/demo1")
+    public String demo1(){
+        return "hello";
+    }
+
+
+}
